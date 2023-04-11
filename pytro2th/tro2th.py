8da287c0ff8c2aba1ac4421bef8d14945d7ad194
Alexis Guizard <47670905+Alexkuark@users.noreply.github.com>
2023-04-11 15:34:35 +0200

    Update tro2th.py

--- pytro2th/tro2th.py
+++ pytro2th/tro2th.py
@@ -408,7 +408,7 @@ def convert_tro(fle_tro_fnme, fle_th_fnme = None, cavename = None,
 	coordinates = None
 	cavename, coordinates, coordsyst, club, entrance, versionfle = read_vtopo_header(lines)
 	
-	if cavename is None or cavename == '' or cavename == ' ':
+	if cavename is None or cavename == '' or cavename == ' ' or cavename == '0.000':
 		cavename = fle_tro_fnme.replace(u'.tro', u'')
 	
 	if fle_th_fnme is None:
